f830d34977044782f8ae464673197d7ffecf343b
Vivi <viviangiacobbi@gmail.com>
2025-07-20 17:08:52 -0500

    Minor fix

--- items/sleeves/sleeve_disc.lua
+++ items/sleeves/sleeve_disc.lua
@@ -6,13 +6,15 @@ local sleeveInfo = {
 }
 
 sleeveInfo.loc_vars = function(self, info_queue)
-    if info_queue then info_queue[#info_queue+1] = {key = "csau_artistcredit", set = "Other", vars = { G.csau_team.gote } } end
+    if info_queue then
+        info_queue[#info_queue+1] = {key = "csau_artistcredit", set = "Other", vars = { G.csau_team.gote } }
+    end
     
     local key = self.key
     self.config = { voucher = 'v_crystal_ball' }
-    if self.get_current_deck_key() == 'b_csau_disc' then
-        key = key..'_alt'
-        self.config.voucher = 'v_csau_foo'
+    if self.get_current_deck_key() == "b_csau_disc" then
+        key = key .. "_alt"
+        self.config.voucher = "v_csau_foo"
     end
 
     local vars = { localize{type = 'name_text', key = self.config.voucher, set = 'Voucher'} }
@@ -21,19 +23,7 @@ end
 
 function sleeveInfo.apply(self, sleeve)
     G.GAME.modifiers.csau_unlimited_stands = true
-
-    if self.get_current_deck_key() == 'b_csau_disc' then
-        G.GAME.starting_params.csau_all_rate = G.GAME.starting_params.csau_all_rate or 1
-        G.GAME.starting_params.csau_all_rate = G.GAME.starting_params.csau_all_rate * sleeve.config.csau_all_rate
-        sleeve.config.active_mod = sleeve.config.probability_mult_alt
-    else
-        sleeve.config.active_mod = sleeve.config.probability_mult
-    end
     CardSleeves.Sleeve.apply(sleeve)
-end
-
-function sleeveInfo.apply(self, back)
-    
 end
 
 return sleeveInfo
